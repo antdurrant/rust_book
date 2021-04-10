809689217f2eaf545d7afe97aaf695733170d25e
Anthony Durrant <antdurrant@gmail.com>
2021-04-10 22:57:16 +0900

    start chapter 3

---
+++ chapter_3/variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+/Users/jprep/Documents/github_personal_projects/rust_book/chapter_3/variables/target/debug/deps/variables: src/main.rs
+
+/Users/jprep/Documents/github_personal_projects/rust_book/chapter_3/variables/target/debug/deps/variables.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_3/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/jprep/Documents/github_personal_projects/rust_book/chapter_3/variables/target/debug/variables: /Users/jprep/Documents/github_personal_projects/rust_book/chapter_3/variables/src/main.rs
